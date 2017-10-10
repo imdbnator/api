8aa07b72314d14248bbb3c30497203eeb215c8b4
Sai Krishna Deep <saikrishna2deep@gmail.com>
2017-10-10 23:48:19 +0530

    Restart existing script on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ssh -t root@139.59.92.196 "cd /srv && git pull origin master && PORT=80 REMOTE=http://www.imdbnator.com pm2 start -f api.js"
+ssh -t root@139.59.92.196 "cd /srv && git pull origin master && PORT=80 REMOTE=http://www.imdbnator.com pm2 restart -f api.js"
